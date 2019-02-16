a4f481e4622aed7a3667768d3f9b2c2adf6f4ebc
Kai Page <kaibug@gmail.com>
2019-02-16 09:30:51 -0600

    Ramping for the drive train

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.drive;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -14,17 +15,24 @@ public class DriveSubsystem extends Subsystem {
     public boolean manualSlow = false;
 
     public DriveSubsystem() {
-        var left = new CANSparkMax(Robot.map.driveLeft[0], MotorType.kBrushless);
-        var right = new CANSparkMax(Robot.map.driveRight[0], MotorType.kBrushless);
+        var left = config(new CANSparkMax(Robot.map.driveLeft[0], MotorType.kBrushless));
+        var right = config(new CANSparkMax(Robot.map.driveRight[0], MotorType.kBrushless));
         drive = new DifferentialDrive(left, right);
 
         for (var i=1; i<Robot.map.driveLeft.length; i++) {
-            new CANSparkMax(Robot.map.driveLeft[i], MotorType.kBrushless).follow(left);
+            config(new CANSparkMax(Robot.map.driveLeft[i], MotorType.kBrushless)).follow(left);
         }
 
         for (var i=1; i<Robot.map.driveRight.length; i++) {
-            new CANSparkMax(Robot.map.driveRight[i], MotorType.kBrushless).follow(right);
+            config(new CANSparkMax(Robot.map.driveRight[i], MotorType.kBrushless)).follow(right);
         }
+    }
+
+    private static CANSparkMax config(CANSparkMax spark) {
+        spark.setIdleMode(IdleMode.kBrake);
+        spark.setRampRate(1); // TODO change to setOpenLoopRampRate after lib update
+        //spark.burnFlash();
+        return spark;
     }
 
     @Override
