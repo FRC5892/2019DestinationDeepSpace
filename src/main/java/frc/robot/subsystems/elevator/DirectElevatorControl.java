3b868d65459a50fbe3e0db9f6f82c3abf84196e1
Kai Page <kaibug@gmail.com>
2019-02-17 11:06:52 -0600

    Oh hey that would have been funny

--- src/main/java/frc/robot/subsystems/elevator/DirectElevatorControl.java
+++ src/main/java/frc/robot/subsystems/elevator/DirectElevatorControl.java
@@ -10,7 +10,7 @@ class DirectElevatorControl extends Command {
     }
 
     private static final double WINCH_SPEED_UP = 1;
-    private static final double WINCH_SPEED_DOWN = -0.2;
+    private static final double WINCH_SPEED_DOWN = 0.2;
 
     @Override
     protected void execute() {
